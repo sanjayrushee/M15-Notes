a30fabe2e4e5df1cd0a069539d2e79406bc51ecb
Sanjay <152201028+sanjayrushee@users.noreply.github.com>
2024-10-03 15:31:02 +0530

    checking navbar in moblie

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -36,32 +36,17 @@ class Navbar extends Component {
     return (
       <>
         {/* Top Navbar with Hamburger Menu */}
-        <div className="flex justify-between md:justify-end p-2 md:p-4">
+        <div className="bg-blue-700 border rounded-xl w-full md:w-4/5 flex justify-center mb-7 hover:bg-gray-100 dark:hover:bg-gray-700 group">
           <button
-            onClick={this.toggleSidebar}
             type="button"
-            className="pl-3 text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            onClick={this.onClickLogout}
+            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white"
           >
-            <span className="sr-only">Open sidebar</span>
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path
-                clipRule="evenodd"
-                fillRule="evenodd"
-                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-              ></path>
-            </svg>
+            <CiLogout className="flex-shrink-0 w-5 h-5 text-white transition duration-75 dark:text-gray-100 group-hover:text-gray-900 dark:group-hover:text-white" />
+            <span className="flex-1 ml-3 whitespace-nowrap">Log out</span>
           </button>
-          <nav className="md:ml-64 text-xl p-2 mt-2 hidden md:block">
-            <p className="text-md font-black md:text-xl">
-              Hello {storedUsername}
-            </p>
-          </nav>
         </div>
+
 
         <aside
           id="default-sidebar"
